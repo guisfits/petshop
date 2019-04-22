4bffe99ff414d1fa8d201b835c52257015186ad1
Guilherme Camargo <guisfits@hotmail.com>
2019-04-21 21:47:41 -0300

    concluindo criação do cliente

--- back-end/src/backoffice/controllers/customer.controller.ts
+++ back-end/src/backoffice/controllers/customer.controller.ts
@@ -1,7 +1,8 @@
+import { HttpStatus } from '@nestjs/common';
 import { AccountService } from './../services/account.service';
 import { CreateCustomerContract } from './../contracts/customer.contract';
 import { ValidatorInterceptor } from './../../core/validator.interceptor';
-import { Controller, Get, Post, Put, Delete, Param, Body, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, UseInterceptors, HttpException } from '@nestjs/common';
 import { Customer } from '../models/customer.model';
 import { Result } from 'src/core/result.model';
 import { CreateCustomerDto } from '../dtos/create-customer.dto';
@@ -10,12 +11,12 @@ import { CustomerService } from '../services/customer.service';
 
 @Controller('v1/customers')
 export class CustomerController {
-    
+
     constructor(
         private readonly accountService: AccountService,
         private readonly customerService: CustomerService
-    ){}
-    
+    ) { }
+
     @Get()
     getAll() {
         return new Result(null, true, [], null);
@@ -29,19 +30,21 @@ export class CustomerController {
     @Post()
     @UseInterceptors(new ValidatorInterceptor(new CreateCustomerContract()))
     async post(@Body() body: CreateCustomerDto) {
-        try{
+        try {
             const userCreated = await this.accountService.create(
                 new User(body.document, body.password, true)
             );
             const customer = await this.customerService.create(
                 new Customer(body.name, body.document, body.email, null, null, null, null, userCreated)
             );
-            
+
             return new Result('Cliente criado com sucesso', true, customer, null);
         }
-        catch(err){
-            console.error(err);
-            return new Result('Ops, algo deu errado', false, null, err);
+        catch (err) {
+            throw new HttpException(
+                new Result('Não foi possível realizar seu cadastro', false, null, err),
+                HttpStatus.BAD_REQUEST
+            );
         }
     }
 
